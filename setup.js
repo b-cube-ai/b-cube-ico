c518e678934b0fda52ac227629b416c7e997e44d
smitrajput <smitrajputtd@gmail.com>
2021-01-29 21:42:09 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -15,6 +15,8 @@ currentTimestamp = Math.floor(Date.now() / 1000);
 // 26 days from today
 openingTime = currentTimestamp + 2246400;
 closingTime = openingTime + 6912000;
+listingTime = closingTime + 6912000;
+twentySixWeeks = 15724800;
 bcube = new web3.eth.Contract(
   CONSTANTS.TOKEN_ABI,
   BCUBEToken.networks[4447].address
@@ -26,17 +28,22 @@ treasury = new web3.eth.Contract(
 usdt = new web3.eth.Contract(CONSTANTS.TETHER_ABI, CONSTANTS.TETHER_ADDRESS);
 async function getAcc() {
   accounts = await web3.eth.getAccounts();
-  await bcube.methods.mint(accounts[0], "1000000000000000000000000").send({
-    from: accounts[0],
-  });
-  await bcube.methods
-    .transfer(
-      "0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5",
-      "500000000000000000000"
-    )
-    .send({
-      from: accounts[0],
-    });
+  // await bcube.methods
+  //   .mint(
+  //     "0x990dFB87BBC04067641D3bb29D23e5FCc7A2A795",
+  //     "50000000000000000000000000"
+  //   )
+  //   .send({
+  //     from: accounts[0],
+  //   });
+  // await bcube.methods
+  //   .transfer(
+  //     "0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5",
+  //     "500000000000000000000"
+  //   )
+  //   .send({
+  //     from: accounts[0],
+  //   });
   await web3.eth.sendTransaction({
     from: accounts[0],
     to: "0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5",
@@ -47,17 +54,20 @@ async function getAcc() {
     .send({
       from: accounts[0],
     });
+  /////////////////////////////////
   await timeMachine.advanceTimeAndBlock(2246400 + 10000);
-  await usdt.methods
-    .approve(Treasury.networks[4447].address, "1000000000000")
-    .send({
-      from: "0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5",
-    });
-  await usdt.methods
-    .transfer("0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5", "21000000000")
-    .send({
-      from: "0xc6cde7c39eb2f0f0095f41570af89efc2c1ea828",
-    });
+  // await timeMachine.advanceTimeAndBlock(twentySixWeeks * 3);
+  /////////////////////////////////
+  //   await usdt.methods
+  //     .approve(Treasury.networks[4447].address, "1000000000000")
+  //     .send({
+  //       from: "0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5",
+  //     });
+  //   await usdt.methods
+  //     .transfer("0xdECB169ebf4A1D5c98066ADa1b9A7069F87CAeB5", "21000000000")
+  //     .send({
+  //       from: "0xc6cde7c39eb2f0f0095f41570af89efc2c1ea828",
+  //     });
 }
 // async function mint() {
 //   await bcube.methods.mint("1000000000000000000000000", accounts[0]);
